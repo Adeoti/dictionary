df47d870cd74d6b1f9908d2e4d9be0e2c78dee4e
Adeoti <adeoti360@gmail.com>
2023-03-13 20:05:54 +0100

    Imported portcapture for netlify

--- src/components/Lemas.jsx
+++ src/components/Lemas.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Lemascards from './Lemascards';
-import myPort from '../images/portcapture.jpg';
+import portcapture from '../images/portcapture.jpg';
 
 const Lemas = () => {
   return (
     <div className='lemas'>
       <div className='bridge'>
         <div className='lemas__cards'>
-            <Lemascards word="Sponsored" image={myPort} desc=""/>
+            <Lemascards word="Sponsored" image={portcapture} desc=""/>
         </div>
       </div>
     </div>
