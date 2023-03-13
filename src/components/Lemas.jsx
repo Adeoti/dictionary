3c294cf90fa3ea698000ba4043ecf0cf41ac7663
Adeoti <adeoti360@gmail.com>
2023-03-13 21:32:14 +0100

    Replaced asset extension

--- src/components/Lemas.jsx
+++ src/components/Lemas.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Lemascards from './Lemascards';
-//import portcapture from '../images/portcapture.JPG';
+import portcapture from '../images/portcaptureb.jpg';
 
 const Lemas = () => {
   return (
     <div className='lemas'>
       <div className='bridge'>
         <div className='lemas__cards'>
-            <Lemascards word="Sponsored" image=""/>
+            <Lemascards word="Sponsored" image={portcapture}/>
         </div>
       </div>
     </div>
